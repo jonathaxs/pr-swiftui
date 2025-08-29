eb6f941b903602f311f3717105030e4ac62406fe
Jonathas Motta <jonathasmrt@gmail.com>
2025-08-29 15:48:09 -0300

    Initial Commit

---
+++ PRSwiftUITests/PRSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  PRSwiftUITests.swift
+//  PRSwiftUITests
+//
+//  Created by Jonathas Motta on 29/08/25.
+//
+
+import Testing
+
+struct PRSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
